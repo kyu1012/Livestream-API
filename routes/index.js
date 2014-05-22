3d7b432be777fe4f78083f94ad27889279e3aba6
kelvin <kyu1012@gmail.com>
2014-05-22 12:27:01 -0400

    fix multiple saves

--- routes/index.js
+++ routes/index.js
@@ -12,44 +12,51 @@ exports.index = function(req, res){
 
 exports.users = function(req, res){
   res.render('users');
-}
+};
 
 exports.submit_data = function(req, res){
   var dirID = req.body.lvsID; //request livestream ID from form
   var apiURL = "https://api.new.livestream.com/accounts/" + dirID; // livestream API Get URL
 
+  //define option parameters and callback function for request call
   var options = {
     url: apiURL,
     'content-type': 'application/json'
   };
   var reqCallback = function(error, response, body) {
     if (!error && response.statusCode == 200) {
-        var info = JSON.parse(body);
-        var livestream_id = dirID;
-        var full_name = info.full_name;
-        var dob = info.dob;
-        var favorite_camera = null;
-        var favorite_movies = null;
+      var info = JSON.parse(body);
+      var livestream_id = dirID;
+      var full_name = info.full_name;
+      var dob = info.dob;
+      var favorite_camera = null;
+      var favorite_movies = null;
 
-        if(info.favorite_camera){
-          favorite_camera = info.favorite_camera;
-        };
-        if(info.favorite_movies){
-          favorite_movies = info.favorite_movies;
-        };
+      if(info.favorite_camera){
+        favorite_camera = info.favorite_camera;
+      };
+      if(info.favorite_movies){
+        favorite_movies = info.favorite_movies;
+      };
 
-        //save information into Director schema
-        var dir = new models.Director({
-          'livestream_id': livestream_id,
-          'full_name': full_name,
-          'dob': dob,
-          'favorite_camera': favorite_camera,
-          'favorite_movies': favorite_movies
-        });
-        dir.save();
+      //save information into Director schema
+      var dir = new models.Director({
+        'livestream_id': livestream_id,
+        'full_name': full_name,
+        'dob': dob,
+        'favorite_camera': favorite_camera,
+        'favorite_movies': favorite_movies
+      });
+
+      models.Director.findOne({"livestream_id": dirID}, function(err, info){
+        if(!info){
+          dir.save();
+        }
+      })
     }
   }
 
+  //query and render user page with updated information after data is saved to mongo
   async.series([
     function(callback){
       request(options, reqCallback);
@@ -65,7 +72,7 @@ exports.submit_data = function(req, res){
     function(err){
     if (err) return next(err);
   })
-}
+};
 
 exports.update_data = function(req, res){
   var movies = req.body.favMovies;
@@ -75,11 +82,10 @@ exports.update_data = function(req, res){
   models.Director.findOneAndUpdate({"full_name": dirName}, {"favorite_camera": camera, "favorite_movies": movies}, function(err, data){
     res.render('users', {"docs": data});
   })
-}
+};
 
 exports.list_data = function(req, res){
   models.Director.find({}, function(err, list_data){
     res.render('users', {"dirs": list_data});
   })
-}
-
+};
